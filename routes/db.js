0a5495c2654035e4395fdec044a6d6b582f3cf1d
adn-total <vtc.total.adn@gmail.com>
2019-07-24 13:58:22 +0100

    first version

---
+++ routes/db.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql')
+
+//connection a la db
+var connection = mysql.createPool({
+    connectionLimit:100,
+    host :  'adnlogistique.com',
+    user : 'rima1',
+    password : 'rima!2017AD',
+    database : 'corfortcar'
+})
+
+/*var connection = mysql.createPool({
+    connectionLimit:100,
+    user : 'root',
+    password : '',
+    database : 'projet'
+})*/
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('accueil db');
+});
+
+/* GET users listing*/
+router.get('/select-client', function(req, res, next) {   
+    //connection.connect();
+    connection.query('SELECT * FROM patient', function (error, results, fields){
+        if(error) throw error;
+        res.send(results) 
+    })
+    //connection.end();
+    
+  //res.send('tous les clients');
+});
+
+
+
+module.exports = router;
